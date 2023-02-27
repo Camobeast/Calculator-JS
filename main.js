04f4d92658d45477c25132e51c027fca362cf02f
Camobeast <jakub.lilley@gmail.com>
2023-02-27 14:22:15 +0100

    Added some calculating functions

--- main.js
+++ main.js
@@ -23,34 +23,43 @@ arr.forEach((element, index) => {
    CALCULATIONS
    ============ */
 
-let tempNumberOne = 0;
-let tempNumberTwo = 0;
-let tempOperator = " ";
+let tempNumberOne = "";
+let tempNumberTwo = "";
+let tempOperator = "";
 let onScreen = 15;
 let screen = document.querySelector('.screen');
 let numberButtons = document.querySelectorAll('.numbers')
 let operatorButtons = document.querySelectorAll('.operators');
 let functionButtons = document.querySelectorAll('.functions');
 let equalButton = document.querySelector('.equal');
+screen.innerText = "0";
+
 
 for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener("click", () => {
-    tempNumberOne = Number(numberButtons[i].innerText);
-    screen.innerText = tempNumberOne + tempOperator + tempNumberTwo;
+    if (screen.innerText === "0") {
+        tempNumberOne = numberButtons[i].innerText;
+        screen.innerText = "";
+        screen.innerText += tempNumberOne;
+    } else {
+        tempNumberOne = numberButtons[i].innerText;
+        screen.innerText += tempNumberOne;
+    }
   });
-}
+};
+
 
 for (let j = 0; j < operatorButtons.length; j++) {
   operatorButtons[j].addEventListener("click", () => {
-    tempOperator = " " + operatorButtons[j].innerText + " ";
-    screen.innerText = tempNumberOne + tempOperator + tempNumberTwo;
+    tempOperator = operatorButtons[j].innerText;
+    screen.innerText += tempOperator;
   });
 }
 
 for (let k = 0; k < functionButtons.length; k++) {
   functionButtons[k].addEventListener("click", () => {
     if (functionButtons[k].innerText == "DEL") {
-
+      return false;
     }
     if (functionButtons[k].innerText == "RESET") {
       location.reload();
@@ -60,7 +69,22 @@ for (let k = 0; k < functionButtons.length; k++) {
 }
 
 equalButton.addEventListener("click", () => {
-  onScreen = tempNumberOne + tempNumberTwo;
-  screen.innerText = onScreen;
-  return;
+  switch (tempOperator) {
+    case "+":
+      result = screen.innerText.split("+");
+      screen.innerText = Number(result[0]) + Number(result[1]);
+      break;
+    case "-":
+      result = screen.innerText.split("-");
+      screen.innerText = Number(result[0]) - Number(result[1]);
+      break;
+    case "x":
+      result = screen.innerText.split("x");
+      screen.innerText = Number(result[0]) * Number(result[1]);
+      break;
+    case "/":
+      result = screen.innerText.split("/");
+      screen.innerText = Number(result[0]) / Number(result[1]);
+      break;
+  }
 });
